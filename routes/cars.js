e16a63ae56ecb69c0e37605790bf9428a336b5b3
sivaShilpa <shilpa.vchand@gmail.com>
2023-03-24 13:51:31 -0400

    complete route for homepage

--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const carsCtrl = require('../controllers/cars')
+const carsCtrl = require('../controllers/cars');
 /* GET users listing. */
 router.get('/', carsCtrl.index);
 
